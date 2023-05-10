9e57fee7167494bc8d9b7799bc233230ce3b6839
Minh Hoang <nmhoangvjppro@gmail.com>
2023-05-10 16:49:54 +0700

    setup finally!

--- src/controllers/site.controller.ts
+++ src/controllers/site.controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 class Site {
 	home(req: Request, res: Response, next: NextFunction) {
-		return res.status(200).json('hello world!!');
+		return res.status(200).render('homepage.ejs');
 	}
 }
 
